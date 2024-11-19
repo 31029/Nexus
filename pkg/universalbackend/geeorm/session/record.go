4d1d15a518998746d83d3699b2880dd7558f357b
h31029 <hzy740876073@gmail.com>
2024-11-19 11:15:36 +0800

    [geeorm] feat: add schema hooks

--- pkg/universalbackend/geeorm/session/record.go
+++ pkg/universalbackend/geeorm/session/record.go
@@ -7,9 +7,11 @@ import (
 	"github.com/31029/nexus/pkg/universalbackend/geeorm/clause"
 )
 
+// Insert one or more records in database
 func (s *Session) Insert(values ...interface{}) (int64, error) {
 	recordValues := make([]interface{}, 0)
 	for _, value := range values {
+		s.CallMethod(BeforeInsert, value)
 		table := s.Model(value).RefTable()
 		s.clause.Set(clause.INSERT, table.Name, table.FieldNames)
 		recordValues = append(recordValues, table.RecordValues(value))
@@ -21,14 +23,15 @@ func (s *Session) Insert(values ...interface{}) (int64, error) {
 	if err != nil {
 		return 0, err
 	}
-
+	s.CallMethod(AfterInsert, nil)
 	return result.RowsAffected()
 }
 
+// Find gets all eligible records
 func (s *Session) Find(values interface{}) error {
+	s.CallMethod(BeforeQuery, nil)
 	destSlice := reflect.Indirect(reflect.ValueOf(values))
 	destType := destSlice.Type().Elem()
-	// 获取切片的单个元素的类型 destType，使用 reflect.New() 方法创建一个 destType 的实例，作为 Model() 的入参，映射出表结构 RefTable()。
 	table := s.Model(reflect.New(destType).Elem().Interface()).RefTable()
 
 	s.clause.Set(clause.SELECT, table.Name, table.FieldNames)
@@ -38,63 +41,33 @@ func (s *Session) Find(values interface{}) error {
 		return err
 	}
 
-	// 遍历每一行记录，利用反射创建 destType 的实例 dest，将 dest 的所有字段平铺开，构造切片 values
 	for rows.Next() {
 		dest := reflect.New(destType).Elem()
 		var values []interface{}
 		for _, name := range table.FieldNames {
 			values = append(values, dest.FieldByName(name).Addr().Interface())
 		}
-		// 调用 rows.Scan() 将该行记录每一列的值依次赋值给 values 中的每一个字段。
 		if err := rows.Scan(values...); err != nil {
 			return err
 		}
-		// 将 dest 添加到切片 destSlice 中。循环直到所有的记录都添加到切片 destSlice 中。
+		s.CallMethod(AfterQuery, dest.Addr().Interface())
 		destSlice.Set(reflect.Append(destSlice, dest))
 	}
 	return rows.Close()
 }
 
-// support map[string]interface{}
-// also support kv list: "Name", "Tom", "Age", 18, ....
-func (s *Session) Update(kv ...interface{}) (int64, error) {
-	m, ok := kv[0].(map[string]interface{})
-	if !ok {
-		m = make(map[string]interface{})
-		for i := 0; i < len(kv); i += 2 {
-			m[kv[i].(string)] = kv[i+1]
-		}
+// First gets the 1st row
+func (s *Session) First(value interface{}) error {
+	dest := reflect.Indirect(reflect.ValueOf(value))
+	destSlice := reflect.New(reflect.SliceOf(dest.Type())).Elem()
+	if err := s.Limit(1).Find(destSlice.Addr().Interface()); err != nil {
+		return err
 	}
-	s.clause.Set(clause.UPDATE, s.RefTable().Name, m)
-	sql, vars := s.clause.Build(clause.UPDATE, clause.WHERE)
-	result, err := s.Raw(sql, vars...).Exec()
-	if err != nil {
-		return 0, err
+	if destSlice.Len() == 0 {
+		return errors.New("NOT FOUND")
 	}
-	return result.RowsAffected()
-}
-
-// Delete records with where clause
-func (s *Session) Delete() (int64, error) {
-	s.clause.Set(clause.DELETE, s.RefTable().Name)
-	sql, vars := s.clause.Build(clause.DELETE, clause.WHERE)
-	result, err := s.Raw(sql, vars...).Exec()
-	if err != nil {
-		return 0, err
-	}
-	return result.RowsAffected()
-}
-
-// Count records with where clause
-func (s *Session) Count() (int64, error) {
-	s.clause.Set(clause.COUNT, s.RefTable().Name)
-	sql, vars := s.clause.Build(clause.COUNT, clause.WHERE)
-	row := s.Raw(sql, vars...).QueryRow()
-	var tmp int64
-	if err := row.Scan(&tmp); err != nil {
-		return 0, err
-	}
-	return tmp, nil
+	dest.Set(destSlice.Index(0))
+	return nil
 }
 
 // Limit adds limit condition to clause
@@ -116,16 +89,49 @@ func (s *Session) OrderBy(desc string) *Session {
 	return s
 }
 
-// 根据传入的类型，利用反射构造切片，调用 Limit(1) 限制返回的行数，调用 Find 方法获取到查询结果。
-func (s *Session) First(value interface{}) error {
-	dest := reflect.Indirect(reflect.ValueOf(value))
-	destSlice := reflect.New(reflect.SliceOf(dest.Type())).Elem()
-	if err := s.Limit(1).Find(destSlice.Addr().Interface()); err != nil {
-		return err
+// Update records with where clause
+// support map[string]interface{}
+// also support kv list: "Name", "Tom", "Age", 18, ....
+func (s *Session) Update(kv ...interface{}) (int64, error) {
+	s.CallMethod(BeforeUpdate, nil)
+	m, ok := kv[0].(map[string]interface{})
+	if !ok {
+		m = make(map[string]interface{})
+		for i := 0; i < len(kv); i += 2 {
+			m[kv[i].(string)] = kv[i+1]
+		}
 	}
-	if destSlice.Len() == 0 {
-		return errors.New("NOT FOUND")
+	s.clause.Set(clause.UPDATE, s.RefTable().Name, m)
+	sql, vars := s.clause.Build(clause.UPDATE, clause.WHERE)
+	result, err := s.Raw(sql, vars...).Exec()
+	if err != nil {
+		return 0, err
 	}
-	dest.Set(destSlice.Index(0))
-	return nil
+	s.CallMethod(AfterUpdate, nil)
+	return result.RowsAffected()
+}
+
+// Delete records with where clause
+func (s *Session) Delete() (int64, error) {
+	s.CallMethod(BeforeDelete, nil)
+	s.clause.Set(clause.DELETE, s.RefTable().Name)
+	sql, vars := s.clause.Build(clause.DELETE, clause.WHERE)
+	result, err := s.Raw(sql, vars...).Exec()
+	if err != nil {
+		return 0, err
+	}
+	s.CallMethod(AfterDelete, nil)
+	return result.RowsAffected()
+}
+
+// Count records with where clause
+func (s *Session) Count() (int64, error) {
+	s.clause.Set(clause.COUNT, s.RefTable().Name)
+	sql, vars := s.clause.Build(clause.COUNT, clause.WHERE)
+	row := s.Raw(sql, vars...).QueryRow()
+	var tmp int64
+	if err := row.Scan(&tmp); err != nil {
+		return 0, err
+	}
+	return tmp, nil
 }
